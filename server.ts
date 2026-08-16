d593be173e43d5e1e6241b4a797e3fffd3c38aef
Madandola james <jamesmadandola@gmail.com>
2026-08-16 05:47:07 +0100

    updated

--- server.ts
+++ server.ts
@@ -29,7 +29,7 @@ app.get('/health', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
